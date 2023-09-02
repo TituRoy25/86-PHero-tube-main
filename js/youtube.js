bb2fb52a746c2ffe817dd4169df9d09768668d46
TituRoy25 <tituroydinajpur11@gmail.com>
2023-09-02 21:14:11 +0600

    some item added

--- js/youtube.js
+++ js/youtube.js
@@ -7,8 +7,8 @@ const handleCategory = async () => {
     data.data.forEach((category) => {
         const div = document.createElement("div");
         div.innerHTML =`
-        <a onclick="handleAll('${category.category_id}')" class="tab">${category.category}</a>
-        `
+        <a onclick="handleAll('${category.category_id}')" class="btn active:bg-[#FF1F3D] ">${category.category}</a>
+        `;
         tabContainer.appendChild(div);
     });
     //console.log(data.data);
@@ -25,25 +25,30 @@ const handleAll = async (categoryId) => {
         //console.log(news);
         const div=document.createElement('div');
         div.innerHTML= `
-        <div class="card">
-            <figure><img class="rounded" src=${news?.thumbnail}/></figure>
-                <div class="card-body grid grid-cols-2">
-                    <div class="w-14 rounded-full">
-                        <img src=${news?.authors[0]?.profile_picture}/>
-                    </div>
-                    
-                    <div class="card-actions">
-                        <h1 class="font-semibold">${news?.title}</h1>
-                        <div class="text-[10px]">
-                            <div class="grid grid-cols-2">
-                                <div><p class="mr-2">${news?.authors[0]?.profile_name}</p></div>
-                                <div><p>${news?.authors[0]?.verified}</p></div>
-                            </div>                    
-                            <p>${news?.others?.views}views</p>
-                        </div>
+            <div class="relative">
+                <figure>
+                <img class=" rounded w-[320px] h-[200px]" src=${news?.thumbnail}/>
+                </figure>
+                <h5 class="absolute bottom-0 right-0 mr-14 mb-3  bg-red-600 text-white text-[10px]">${news?.others?.posted_date}ago</h5>
+            </div>
+
+            <div class="mt-2 flex flex-row">
+                <div class="w-[30px] h-[32px] rounded-full">
+                    <img src=${news?.authors[0]?.profile_picture}/>
+                </div>
+                
+                <div class="ml-5">
+                    <h1 class="font-bold text-[20px]">${news?.title}</h1>
+                    <div class="text-[15px]">
+                        <div class="flex flex-row">
+                            <div><p class="mr-2">${news?.authors[0]?.profile_name}</p></div>
+                            <div><p>${news?.authors[0]?.verified}</p></div>
+                        </div>                    
+                        <p>${news?.others?.views} views</p>
                     </div>
                 </div>
-        </div>`
+            </div>
+            `;
 
         cardContainer.appendChild(div);
     })
@@ -53,5 +58,5 @@ const handleAll = async (categoryId) => {
 
 
 
-
 handleCategory();
+handleAll(1000);
